ecb19f9b3582a6aa686871148640bb7e7be9e108
Alex <alex.fruzenshtein@gmail.com>
2015-01-20 22:33:10 +0200

    CRUD Experience done

--- app/controllers/LawyerExperienceController.scala
+++ app/controllers/LawyerExperienceController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import com.wordnik.swagger.annotations._
 import forms.UserAccountForms
+import models.Experience
 import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.Controller
@@ -65,6 +66,74 @@ object LawyerExperienceController extends Controller with Security with UserAcco
         case Some(experience) => Future.successful(Ok(Json.toJson(experience)))
         case None => Future.successful(NotFound(Json.obj("message" -> "Experience does not exist")))
       }
+  }
+
+  @ApiOperation(
+    nickname = "lawyersExperience",
+    value = "Put lawyers experience",
+    notes = "Put lawyers experience",
+    httpMethod = "PUT",
+    response = classOf[models.swagger.InformationMessage])
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Experience successfully updated"),
+    new ApiResponse(code = 400, message = "Validation errors"),
+    new ApiResponse(code = 404, message = "Experience with such id does not exist")
+  ))
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "Authorization", value = "Header parameter. Example 'Bearer yourTokenHere'.", dataType = "string", paramType = "header", required = true),
+    new ApiImplicitParam(value = "Lawyer Experience object which will be updated", required = true, dataType = "models.swagger.Experience", paramType = "body")
+  ))
+  def updateExperience(id: String) = isAuthenticated { implicit acc =>
+    implicit request =>
+      createExperienceForm.bindFromRequest fold(
+        formWithErrors => {
+          Logger.info("Experience data was with ERRORS")
+          Future(BadRequest(Json.obj("message" -> formWithErrors.errorsAsJson)))
+        },
+        newExperience => {
+          acc.experience match {
+            case Some(experience) => {
+              experience.find((e: Experience) => e.id == Some(id)) match {
+                case Some(exp) => {
+                  LawyerService.deleteExperience(acc.email, id)
+                  LawyerService.createExperience(acc.email, models.Experience.generateExperience(newExperience))
+                  Future.successful(Ok(Json.obj("message" -> "Experience successfully updated")))
+                }
+                case None => Future.successful(NotFound(Json.obj("message" -> s"Experience with id $id does not exist")))
+              }
+            }
+            case None => Future.successful(Ok(Json.obj("message" -> "Experience does not exist")))
+          }
+        }
+      )
+  }
+
+  @ApiOperation(
+    nickname = "lawyersExperience",
+    value = "Delete lawyers experience by id",
+    notes = "Delete lawyers experience by id",
+    httpMethod = "DELETE",
+    response = classOf[models.swagger.InformationMessage])
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Experience successfully deleted"),
+    new ApiResponse(code = 404, message = "Experience with such id does not exist")
+  ))
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "Authorization", value = "Header parameter. Example 'Bearer yourTokenHere'.", dataType = "string", paramType = "header", required = true)
+  ))
+  def deleteExperience(id: String) = isAuthenticated { implicit acc =>
+    implicit request => acc.experience match {
+      case Some(exp) => {
+        exp.find((e: Experience) => e.id == Some(id)) match {
+          case Some(_) => {
+            LawyerService.deleteExperience(acc.email, id)
+            Future.successful(Ok(Json.obj("message" -> s"Experience with id: $id successfully deleted")))
+          }
+          case None => Future.successful(NotFound(Json.obj("message" -> "Experience does not exist")))
+        }
+      }
+      case None => Future.successful(Ok(Json.obj("message" -> "Experience does not exist")))
+    }
   }
 
 }
